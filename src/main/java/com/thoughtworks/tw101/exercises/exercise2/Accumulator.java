604ef88223ef1ed6d3f4ad4923083940afa3af96
PatrickDale <patdale216@gmail.com>
2015-07-13 17:48:26 -0700

    Excercise2 finished

--- src/main/java/com/thoughtworks/tw101/exercises/exercise2/Accumulator.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise2/Accumulator.java
@@ -1,4 +1,13 @@
 package com.thoughtworks.tw101.exercises.exercise2;
 
 public class Accumulator {
+    private int accumulator;
+
+    public void increment() {
+        accumulator ++;
+    }
+
+    public void total() {
+        System.out.println(accumulator);
+    }
 }
