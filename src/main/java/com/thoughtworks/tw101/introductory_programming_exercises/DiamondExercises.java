b9a5303ea64f5a9705892a84cf39b299f238f653
PatrickDale <patdale216@gmail.com>
2015-07-22 15:51:58 -0700

    DiamondExercises: drawAnIsoscelesTraingle finished. Need to refactor.

--- src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/DiamondExercises.java
+++ src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/DiamondExercises.java
@@ -7,16 +7,49 @@ public class DiamondExercises {
         drawADiamondWithYourName(3);
     }
 
+    //Helper Methods
+    private static int getNumOfAsteriskForLevel(int level) {
+        return (level * 2) + 1;
+    }
+
+    private static String returnAHorizontalLine(int numOfAsteriskForLevel) {
+        String line = "";
+        for (int i = 0; i < numOfAsteriskForLevel; i++) {
+            line += "*";
+        }
+        return line;
+    }
+
 //    Isosceles Triangle
 //    Given a number n, print a centered triangle. Example for n=3:
 //              *
 //             ***
 //            *****
     private static void drawAnIsoscelesTriangle(int n) {
+        int charsPerLevel = (n * 2) - 1;
+
+        for (int i = 0; i < n; i++) {
+            int numOfAsteriskForLevel = getNumOfAsteriskForLevel(i);
+            String line = returnAHorizontalLine(numOfAsteriskForLevel);
+            int padding = getPaddingAmountForLevel(charsPerLevel, numOfAsteriskForLevel);
+            line = addPaddingToLine(line, padding);
+            System.out.println(line);
+        }
 
     }
 
-//    Diamond
+    private static String addPaddingToLine(String line, int padding) {
+        for (int i = 0; i < padding; i++) {
+            line = " " + line + " ";
+        }
+        return line;
+    }
+
+    private static int getPaddingAmountForLevel(int charsPerLevel, int numOfAsteriskForLevel) {
+        return (charsPerLevel - numOfAsteriskForLevel) / 2;
+    }
+
+    //    Diamond
 //    Given a number n, print a centered diamond. Example for n=3:
 //              *
 //             ***
