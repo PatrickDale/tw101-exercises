76e725ab9d7b2c47f84407e7873c7545a86fb299
PatrickDale <patdale216@gmail.com>
2015-07-23 13:55:10 -0700

    PrimeFactors almost complete. Need to refactor to reduce repetitive code and remove code smells.

--- src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/PrimeFactors.java
+++ src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/PrimeFactors.java
@@ -6,14 +6,46 @@ package com.thoughtworks.tw101.introductory_programming_exercises;
 //
 //  For example, generate(1) should return an empty list and generate(30) should return the numbers: 2,3,5.
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PrimeFactors {
     public static void main(String[] args) {
-        List<Integer> primeFactors = generate(30);
+        List<Integer> primeFactors = generate(100);
+        print(primeFactors);
     }
 
+    private static void print(List<Integer> primeFactors) {
+        for (int e : primeFactors) {
+            System.out.println(e);
+        }
+    }
+
+    // Idea based on : http://www.quora.com/Is-there-a-relation-between-a-number-and-its-largest-prime-factor
     private static List<Integer> generate(int n) {
-        return null;
+        List<Integer> primeFactors = new ArrayList<>();
+        if (n > 3) {
+            int upperPrimeFactorBound = getUpperPrimeFactorBound(n);
+            for (int i = 2; i <= upperPrimeFactorBound; i++) {
+                if (n % i == 0 && isPrime(i, primeFactors)) {
+                    primeFactors.add(i);
+                    upperPrimeFactorBound = n / i;
+                }
+            }
+        }
+        return primeFactors;
+    }
+
+    private static boolean isPrime(int number, List<Integer> primeFactors) {
+        for (int n: primeFactors) {
+            if (number % n == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static int getUpperPrimeFactorBound(int n) {
+        return n/2;
     }
 }
